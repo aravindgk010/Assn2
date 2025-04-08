dd8a7cd655c3382521d8b755cc181bee88dedc17
Aravind Gopal <gopa0025@algonquinlive.com>
2025-04-08 15:50:42 -0400

    edited create_red_ball

--- aisd_examples/aisd_examples/envs/create_red_ball.py
+++ aisd_examples/aisd_examples/envs/create_red_ball.py
@@ -5,7 +5,7 @@ import numpy as np
 
 import rclpy
 from rclpy.node import Node
-import sensor_msgs.msg import Image
+from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge
 import cv2
