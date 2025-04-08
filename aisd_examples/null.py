f5214aaf9dfc98eb84bd55519454edcda0975dd2
Aravind Gopal <gopa0025@algonquinlive.com>
2025-04-08 15:39:34 -0400

    added null agent

---
+++ aisd_examples/null.py
@@ -0,0 +1,21 @@
+import time
+import gymnasium as gym
+import aisd_examples
+
+# Create the environment
+env = gym.make("aisd_examples/RedBall-V0", render_mode="human")
+
+# Reset the environment
+observation, info = env.reset()
+
+
+# Run the agent for 100 steps
+for step in range(1000):
+    action = env.action_space.sample()  # Random action selection
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        print("Episode finished, resetting environment...")
+        observation, info = env.reset()
+
+env.close()
